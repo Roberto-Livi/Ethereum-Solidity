def5679fa832d62891da31b3d2a8b85d950a0c05
Roberto-Livi <robertomlivi@outlook.com>
2022-05-11 22:24:13 -0400

    adding files

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 =  require('web3'); // capitalized because it is a constructor function
+const web3 = new Web3(ganache.provider()); // instance of web3 to connect to local test network
+const { interface, bytecode } = require('../compile'); 
+
+let accounts;
+
+beforeEach(async () => {
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    // use one of those accounts to deploy contract
+
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts)
+    });
+});
